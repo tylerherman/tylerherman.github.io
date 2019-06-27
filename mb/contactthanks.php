6917bb3ba7a27384cde7dde056c0b18369ae90b1
Tyler Herman <contact@tylerherman.com>
2019-06-26 22:42:36 -0500

    update to marians site for her to take a look at

---
+++ mb/contactthanks.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Contact Marian Brady</title>
+  <meta name="description" content="Join the Brady Bunch to help elect Marian Brady to the 4th Harbor District">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="../css/normalize.css">
+  <link rel="stylesheet" href="../css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet">
+
+</head>
+
+<body>
+<div class="wrap">
+<h1><img src="../img/marian-brady.png" alt="Marian Brady for 4th Harbor District" /></h1>
+<nav>
+<ul>
+    <li><a href="http://marianbradyforharborcommission.com">Home</a></li>
+    <li><a href="http://marianbradyforharborcommission.com/endorsements">Endorsments</a></li>
+    <li><a href="http://marianbradyforharborcommission.com/contact">Contact</a></li>
+</ul>
+</nav>
+    
+<div class="hintro">
+    <h2>Message Sent</h2>
+    <p>Thanks for getting in touch. We'll get back to you soon.</p>
+</div>
+
+<h6>To lend your support you can contact Marian Brady at:</h6>
+<p><strong>Phone:</strong> 707-443-8282 or 707-599-7447<br /><strong>Email:</strong> <a href="mailto:marianbradyforharborcommission@gmail.com">marianbradyforharborcommission@gmail.com</a><br /><strong>Address:</strong> P.O. Box 6024, Eureka, CA 95502</p>
+
+<footer>
+    <p>Marian Brady for HBHRC District, Division 4 FPPC #1409437 P.O. Box 6024, Eureka, CA 95502</p>
+</footer>
+
+</div>
+
+
+
+<script src="../js/vendor/modernizr-3.6.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+<script src="../js/plugins.js"></script>
+<script src="../js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+  window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+  ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
